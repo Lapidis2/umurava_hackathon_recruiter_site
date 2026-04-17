bdfee7d67e23c5f0be54b72a25b7011619c63b76
Lapidis2 <hitlapidis2@gmail.com>
2026-04-18 00:38:05 +0200

    project scafolding

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className='bg-blue-600 h-screen '>
+      <h1 className='font-extrabold text-7xl'>Hello World</h1>
+    </div>
+  )
+}
+
+export default page
